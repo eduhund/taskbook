4d67ebbbf8a90731b8bd0bdc13840387a25dd06a
Roman Nebel <im.roman.nebel@gmail.com>
2023-03-29 16:29:19 +0200

    Fix auth

--- src/modules/apiRequests/authUser/authUser.js
+++ src/modules/apiRequests/authUser/authUser.js
@@ -7,42 +7,42 @@ const { checkPass } = require("../../../utils/pass");
 const tokens = accessTokens;
 
 async function authUser({ email, pass, lang }) {
-  const user = await getDBRequest("getUserInfo", {
-    query: { email },
-  });
-  if (user) {
-    if (checkPass(user, pass)) {
-      const userToken = tokens.setToken(user);
-      lang !== user.lang &&
-        getDBRequest("setUserInfo", { email, data: { lang } });
-      log.info(`Auth success!`);
-      return {
-        status: 0,
-        data: {
-          id: user.id,
-          email: user.email,
-          firstName: user.firstName,
-          lastName: user.lastName,
-          lang,
-          token: userToken,
-        },
-      };
-    } else {
-      log.info(`Password is invalid!`);
-      return {
-        status: 10002,
-        error: "invalid_credentials",
-        error_description: "Invalid password",
-      };
-    }
-  } else {
-    log.info(`User ${email} didn't found!`);
-    return {
-      status: 10001,
-      error: "invalid_credentials",
-      error_description: "User didn't found",
-    };
-  }
+	const user = await getDBRequest("getUserInfo", {
+		query: { email },
+	});
+	if (user) {
+		if (checkPass(user, pass)) {
+			const userToken = tokens.setToken(user);
+			lang !== user.lang &&
+				getDBRequest("setUserInfo", { email, data: { lang } });
+			log.info(`Auth success!`);
+			return {
+				OK: true,
+				data: {
+					id: user.id,
+					email: user.email,
+					firstName: user.firstName,
+					lastName: user.lastName,
+					lang,
+					token: userToken,
+				},
+			};
+		} else {
+			log.info(`Password is invalid!`);
+			return {
+				status: 10002,
+				error: "invalid_credentials",
+				error_description: "Invalid password",
+			};
+		}
+	} else {
+		log.info(`User ${email} didn't found!`);
+		return {
+			status: 10001,
+			error: "invalid_credentials",
+			error_description: "User didn't found",
+		};
+	}
 }
 
 module.exports.authUser = authUser;
