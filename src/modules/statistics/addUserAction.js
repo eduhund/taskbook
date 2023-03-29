43722ed38e0fada19e2936f0b86a5c191a27ec38
Roman Nebel <im.roman.nebel@gmail.com>
2023-03-29 11:41:42 +0200

    Update gitignore Delete unneccessary logs

--- src/modules/statistics/addUserAction.js
+++ src/modules/statistics/addUserAction.js
@@ -2,23 +2,22 @@ const { log } = require("../../utils/logger");
 const { getDBRequest } = require("../dbRequests/dbRequests");
 
 async function addUserAction({
-  userId = "U0000000",
-  action = "default",
-  data = {},
-  req = {},
+	userId = "U0000000",
+	action = "default",
+	data = {},
+	req = {},
 }) {
-  console.log(req.headers);
-  const params = {};
-  try {
-    await getDBRequest("addUserAction", {
-      userId,
-      action,
-      data,
-      params,
-    });
-  } catch (e) {
-    log.warn(e);
-  }
+	const params = {};
+	try {
+		await getDBRequest("addUserAction", {
+			userId,
+			action,
+			data,
+			params,
+		});
+	} catch (e) {
+		log.warn(e);
+	}
 }
 
 module.exports.addUserAction = addUserAction;
--- src/utils/generateCertId.js
+++ src/utils/generateCertId.js
@@ -2,36 +2,34 @@ const { db } = require("../modules/dbRequests/mongo");
 const { log } = require("../utils/logger");
 
 function padder(number = 0, count = 1) {
-  return number.toString().padStart(count, "0");
+	return number.toString().padStart(count, "0");
 }
 
 async function generateCertId(userId, moduleId, startDate) {
-  const certsCount =
-    (await db.CERTS.countDocuments({ moduleId, startDate })) || 0;
+	const certsCount =
+		(await db.CERTS.countDocuments({ moduleId, startDate })) || 0;
 
-  const postfix = padder(certsCount + 1, 4);
-  const date = new Date(Date.parse(startDate));
-  const datePart = `${padder(date.getFullYear() - 2000, 2)}${padder(
-    date.getMonth(),
-    2
-  )}`;
-  const certId = `${moduleId}${datePart}${postfix}`;
+	const postfix = padder(certsCount + 1, 4);
+	const date = new Date(Date.parse(startDate));
+	const datePart = `${padder(date.getFullYear() - 2000, 2)}${padder(
+		date.getMonth(),
+		2
+	)}`;
+	const certId = `${moduleId}${datePart}${postfix}`;
 
-  db.CERTS.insertOne({
-    id: certId,
-    userId,
-    moduleId,
-    startDate,
-    public: false,
-  }).then((result) => console.log(result));
+	db.CERTS.insertOne({
+		id: certId,
+		userId,
+		moduleId,
+		startDate,
+		public: false,
+	});
 
-  const path = `modules.${moduleId}.certId`;
+	const path = `modules.${moduleId}.certId`;
 
-  db.USERS.findOneAndUpdate({ id: userId }, { $set: { [path]: certId } }).then(
-    (result) => console.log(result)
-  );
+	db.USERS.findOneAndUpdate({ id: userId }, { $set: { [path]: certId } });
 
-  return certId;
+	return certId;
 }
 
 module.exports.generateCertId = generateCertId;
