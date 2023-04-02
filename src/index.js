d3a8fdc93c11e56c11f29b9c45b0f221663cfcd5
Roman Nebel <im.roman.nebel@gmail.com>
2023-04-02 12:29:16 +0200

    Polish index file

--- src/index.js
+++ src/index.js
@@ -6,14 +6,17 @@ const https = require("https");
 const { log } = require("./utils/logger");
 const { app } = require("./services/express");
 
-const port = process.env.SERVER_PORT;
+const port = process.env.SERVER_PORT || 443;
 
 const options = {};
+
 try {
 	options.cert = fs.readFileSync(process.env.SSL_CERT);
 	options.key = fs.readFileSync(process.env.SSL_KEY);
 } catch (e) {
-	log.error("Can't download SSL certificates: ", e);
+	log.error("Can't download SSL certificates!");
+	log.debug(e);
+	process.exit();
 }
 
 const server = https.createServer(options, app);
