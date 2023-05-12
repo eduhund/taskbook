e7dab231b3479d701f3e9b40ef61c7a75cf747ed
Roman Nebel <im.roman.nebel@gmail.com>
2023-05-12 18:48:20 +0200

    Decorate check credentials process

--- src/processes/checkCredentials/checkCredentials.js
+++ src/processes/checkCredentials/checkCredentials.js
@@ -1,11 +1,18 @@
-const { log } = require("@logger");
+const { checkPass } = require("@utils/pass");
 
-const { checkPass } = require("../../utils/pass");
-
+/***
+ * Function checks user's credentials.
+ *
+ * @param {Object} data Throught API object
+ * @param {Function} next Express middleware next function
+ *
+ * @returns {boolean} Check result
+ */
 async function checkCredentials(data, next) {
 	const { user, pass } = data;
-	if (!checkPass(user, pass)) {
-		log.info(`${user.email}: Invalid password!`);
+	const isPassValid = checkPass(user, pass);
+
+	if (isPassValid) {
 		next({ code: 10102 });
 		return false;
 	}
