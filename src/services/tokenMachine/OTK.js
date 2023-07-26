740544e9638ce72f50f099035407cd806b846e71
Roman Nebel <im.roman.nebel@gmail.com>
2023-07-26 17:16:00 +0200

    Update OTK endpoints

--- src/services/tokenMachine/OTK.js
+++ src/services/tokenMachine/OTK.js
@@ -3,19 +3,19 @@ const fetch = require("node-fetch");
 const server = process.env.REG_OTK_SERVER;
 
 async function checkKey(key) {
-	const response = await fetch(`${server}/check_otk?key=${key}`);
+	const response = await fetch(`${server}/checkKey?key=${key}`);
 	const data = await response.json();
-	return data || false;
+	return data?.OK || false;
 }
 
 async function setKey(user) {
-	const response = await fetch(`${server}/set_otk`, {
+	const response = await fetch(`${server}/setKey`, {
 		method: "POST",
 		body: JSON.stringify(user),
 		headers: { "Content-Type": "application/json" },
 	});
 	const data = await response.json();
-	return data || undefined;
+	return data?.data?.key || undefined;
 }
 
 module.exports = { checkKey, setKey };
