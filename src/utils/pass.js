6700225561bec05f43d5cb20f83698d06ce1fa6a
Roman Nebel <im.roman.nebel@gmail.com>
2023-08-11 20:40:52 +0200

    Update pass checking

--- src/utils/pass.js
+++ src/utils/pass.js
@@ -1,13 +1,12 @@
 const { createHash } = require("crypto");
 
-function hashPass(pass) {
+function hashPass(pass = "") {
 	return createHash("sha256").update(pass).digest("hex");
 }
 
-function checkPass(user, pass) {
-	const validity = user?.pass === pass ? true : false;
-	return validity;
+function checkPass(user = {}, pass = "") {
+	if (typeof user.pass !== "undefined" && pass.length > 0)
+		return user.pass === pass;
 }
 
-module.exports.hashPass = hashPass;
-module.exports.checkPass = checkPass;
+module.exports = { hashPass, checkPass };
