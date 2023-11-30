a27d25d775618939a5669dbf85072bb2f91ee2ea
Roman Nebel <im.roman.nebel@gmail.com>
2023-11-30 13:50:05 +0100

    Update OTK machine

--- src/services/tokenMachine/OTK.js
+++ src/services/tokenMachine/OTK.js
@@ -1,18 +1,51 @@
 const fetch = require("node-fetch");
 
-const server = process.env.REG_OTK_SERVER;
+const { OTK_SERVER, OTK_TOKEN } = process.env;
+
+const bearerToken = "Bearer " + OTK_TOKEN
+
+function getKeyParams(type) {
+	switch (type) {
+		case "oneTimePass":
+			return {
+				type: "digit",
+				length: 4,
+				life: 3600,
+				isReusable: false
+			}
+		
+		default:
+			return {
+				type: "digit",
+				length: 4,
+				life: 3600,
+				isReusable: false
+			}
+	}
+}
 
 async function checkKey(key) {
-	const response = await fetch(`${server}/checkKey?key=${key}`);
+	const response = await fetch(`${OTK_SERVER}/checkKey?key=${key}`, {
+		headers: { 
+			"Authorization": bearerToken
+		 },
+	});
 	const data = await response.json();
 	return data?.OK || false;
 }
 
-async function setKey(user) {
-	const response = await fetch(`${server}/setKey`, {
+async function setKey(userId, type) {
+	const body = {
+		userId,
+		...getKeyParams(type)
+	}
+	const response = await fetch(`${OTK_SERVER}/setKey`, {
 		method: "POST",
-		body: JSON.stringify(user),
-		headers: { "Content-Type": "application/json" },
+		body: JSON.stringify(body),
+		headers: { 
+			"Content-Type": "application/json",
+			"Authorization": bearerToken
+		 },
 	});
 	const data = await response.json();
 	return data?.data?.key || undefined;
