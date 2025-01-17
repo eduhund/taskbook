d3e65f1be3b42b5446f44e705edac5ecae2c5c51
Roman Nebel <im.roman.nebel@gmail.com>
2025-01-17 09:09:49 +0100

    Add API error handler

--- backend/src/services/express/express.js
+++ backend/src/services/express/express.js
@@ -5,14 +5,15 @@ const { log } = require("@logger");
 const { PUBLIC, TEACHER } = require("../../modules/apiRequests/apiRequests");
 const { checkAdmin } = require("./security");
 const { paramsProcessor } = require("../../utils/validate");
+const { responseHandler } = require("./responses");
 
 const { SERVER_PORT = 8888, ORIGIN = "*" } = process.env;
 
 const app = express();
 
 const corsOptions = {
-	origin: ORIGIN,
-	optionsSuccessStatus: 200,
+  origin: ORIGIN,
+  optionsSuccessStatus: 200,
 };
 
 app.use(cors(corsOptions));
@@ -26,38 +27,39 @@ const student = express.Router();
 app.use("/student", student);
 student.use(paramsProcessor);
 for (const request of PUBLIC) {
-	const { path, method, exec } = request;
-	switch (method) {
-		case "get":
-			student.get(path, exec);
-		case "post":
-			student.post(path, exec);
-	}
+  const { path, method, exec } = request;
+  switch (method) {
+    case "get":
+      student.get(path, exec);
+    case "post":
+      student.post(path, exec);
+  }
 }
 
 const oldTeacher = express.Router();
 app.use("/teacher", oldTeacher);
 oldTeacher.use(checkAdmin);
 for (const request of TEACHER) {
-	const { path, method, exec } = request;
-	switch (method) {
-		case "get":
-			oldTeacher.get(path, exec);
-		case "post":
-			oldTeacher.post(path, exec);
-	}
+  const { path, method, exec } = request;
+  switch (method) {
+    case "get":
+      oldTeacher.get(path, exec);
+    case "post":
+      oldTeacher.post(path, exec);
+  }
+  oldTeacher.use(responseHandler);
 }
 
 function start() {
-	return new Promise((resolve, reject) => {
-		app.listen(SERVER_PORT, (err) => {
-			if (err) {
-				return reject(err);
-			}
-			log.info("Server starts on port", SERVER_PORT);
-			return resolve();
-		});
-	});
+  return new Promise((resolve, reject) => {
+    app.listen(SERVER_PORT, (err) => {
+      if (err) {
+        return reject(err);
+      }
+      log.info("Server starts on port", SERVER_PORT);
+      return resolve();
+    });
+  });
 }
 
 module.exports = { start };
--- backend/src/utils/messageGenerator.js
+++ backend/src/utils/messageGenerator.js
@@ -1,162 +1,167 @@
 const ERRORS = [
-	{
-		code: -1,
-		type: "other_error",
-	},
-	{
-		code: 10001,
-		type: "invalid_request",
-		description: "Invalid path",
-	},
-	{
-		code: 10002,
-		type: "invalid_request",
-		description: "Missing required params",
-	},
-	{
-		code: 10101,
-		type: "invalid_credentials",
-		description: "User didn't found",
-	},
-	{
-		code: 10102,
-		type: "invalid_credentials",
-		description: "Invalid password",
-	},
-	{
-		code: 10103,
-		type: "invalid_credentials",
-		description: "Verify key is invalid or expired",
-	},
-	{
-		code: 10104,
-		type: "invalid_credentials",
-		description: "Payment didn't found",
-	},
-	{
-		code: 10105,
-		type: "invalid_credentials",
-		description: "Access token is invalid or expired",
-	},
-	{
-		code: 10201,
-		type: "access_denied",
-		description: "You don't have access to this content",
-	},
-	{
-		code: 10301,
-		type: "not_found",
-		description: "Task with this ID is not exist",
-	},
-	{
-		code: 10302,
-		type: "not_found",
-		description: "Lesson with this ID is not exist",
-	},
-	{
-		code: 10303,
-		type: "not_found",
-		description: "Module with this ID is not exist",
-	},
-	{
-		code: 20101,
-		type: "process_failure",
-		description: "Password didn't set",
-	},
-	{
-		code: 20102,
-		type: "process_failure",
-		description: "Error while preparing task data",
-	},
-	{
-		code: 20103,
-		type: "process_failure",
-		description: "Error while preparing module start data",
-	},
-	{
-		code: 20104,
-		type: "process_failure",
-		description: "Error while preparing module final data",
-	},
-	{
-		code: 20105,
-		type: "process_failure",
-		description: "Error while preparing lesson start data",
-	},
-	{
-		code: 20106,
-		type: "process_failure",
-		description: "Error while preparing lesson final data",
-	},
-	{
-		code: 20107,
-		type: "process_failure",
-		description: "Error while preparing dashboard data",
-	},
-	{
-		code: 20108,
-		type: "process_failure",
-		description: "Error while preparing lessons list",
-	},
-	{
-		code: 20109,
-		type: "process_failure",
-		description: "Error while preparing tasks list",
-	},
-	{
-		code: 20110,
-		type: "process_failure",
-		description: "Error while preparing module info",
-	},
-	{
-		code: 20111,
-		type: "process_failure",
-		description: "Error while preparing diploma",
-	},
-	{
-		code: 20112,
-		type: "process_failure",
-		description: "Error while setting new question state",
-	},
-	{
-		code: 20113,
-		type: "process_failure",
-		description: "Error while task was checking",
-	},
-	{
-		code: 20114,
-		type: "process_failure",
-		description: "Error while updating task's controls state",
-	},
-	{
-		code: 20115,
-		type: "process_failure",
-		description: "Error with processing new comment",
-	},
-	{
-		code: 20116,
-		type: "process_failure",
-		description: "Error with getting counselor content",
-	},
-	{
-		code: 20117,
-		type: "process_failure",
-		description: "Error with updating comment status",
-	},
-	{
-		code: 20118,
-		type: "process_failure",
-		description: "Error with resetting user password",
-	},
+  {
+    code: -1,
+    type: "other_error",
+  },
+  {
+    code: 10001,
+    type: "invalid_request",
+    description: "Invalid path",
+  },
+  {
+    code: 10002,
+    type: "invalid_request",
+    description: "Missing required params",
+  },
+  {
+    code: 10101,
+    type: "invalid_credentials",
+    description: "User didn't found",
+  },
+  {
+    code: 10102,
+    type: "invalid_credentials",
+    description: "Invalid password",
+  },
+  {
+    code: 10103,
+    type: "invalid_credentials",
+    description: "Verify key is invalid or expired",
+  },
+  {
+    code: 10104,
+    type: "invalid_credentials",
+    description: "Payment didn't found",
+  },
+  {
+    code: 10105,
+    type: "invalid_credentials",
+    description: "Access token is invalid or expired",
+  },
+  {
+    code: 10201,
+    type: "access_denied",
+    description: "You don't have access to this content",
+  },
+  {
+    code: 10202,
+    type: "access_denied",
+    description: "You don't have access to this content",
+  },
+  {
+    code: 10301,
+    type: "not_found",
+    description: "Task with this ID is not exist",
+  },
+  {
+    code: 10302,
+    type: "not_found",
+    description: "Lesson with this ID is not exist",
+  },
+  {
+    code: 10303,
+    type: "not_found",
+    description: "Module with this ID is not exist",
+  },
+  {
+    code: 20101,
+    type: "process_failure",
+    description: "Password didn't set",
+  },
+  {
+    code: 20102,
+    type: "process_failure",
+    description: "Error while preparing task data",
+  },
+  {
+    code: 20103,
+    type: "process_failure",
+    description: "Error while preparing module start data",
+  },
+  {
+    code: 20104,
+    type: "process_failure",
+    description: "Error while preparing module final data",
+  },
+  {
+    code: 20105,
+    type: "process_failure",
+    description: "Error while preparing lesson start data",
+  },
+  {
+    code: 20106,
+    type: "process_failure",
+    description: "Error while preparing lesson final data",
+  },
+  {
+    code: 20107,
+    type: "process_failure",
+    description: "Error while preparing dashboard data",
+  },
+  {
+    code: 20108,
+    type: "process_failure",
+    description: "Error while preparing lessons list",
+  },
+  {
+    code: 20109,
+    type: "process_failure",
+    description: "Error while preparing tasks list",
+  },
+  {
+    code: 20110,
+    type: "process_failure",
+    description: "Error while preparing module info",
+  },
+  {
+    code: 20111,
+    type: "process_failure",
+    description: "Error while preparing diploma",
+  },
+  {
+    code: 20112,
+    type: "process_failure",
+    description: "Error while setting new question state",
+  },
+  {
+    code: 20113,
+    type: "process_failure",
+    description: "Error while task was checking",
+  },
+  {
+    code: 20114,
+    type: "process_failure",
+    description: "Error while updating task's controls state",
+  },
+  {
+    code: 20115,
+    type: "process_failure",
+    description: "Error with processing new comment",
+  },
+  {
+    code: 20116,
+    type: "process_failure",
+    description: "Error with getting counselor content",
+  },
+  {
+    code: 20117,
+    type: "process_failure",
+    description: "Error with updating comment status",
+  },
+  {
+    code: 20118,
+    type: "process_failure",
+    description: "Error with resetting user password",
+  },
 ];
 
 function generateMessage(code, data) {
-	if (code === 0) {
-		return { OK: true, data };
-	} else {
-		const error = ERRORS.find((error) => error.code === code) || -1;
-		return { OK: false, error };
-	}
+  if (code === 0) {
+    return { OK: true, data };
+  } else {
+    const error = ERRORS.find((error) => error.code === code) || -1;
+    return { OK: false, error };
+  }
 }
 
 module.exports = { generateMessage };
