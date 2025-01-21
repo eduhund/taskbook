b0b2bb286d871829519f59fcd9ba02c5ab61cc6c
Roman Nebel <im.roman.nebel@gmail.com>
2025-01-21 09:58:50 +0100

    Fix auth

--- backend/src/utils/pass.js
+++ backend/src/utils/pass.js
@@ -1,12 +1,12 @@
 const { createHash } = require("crypto");
 
 function hashPass(pass) {
-	return createHash("sha256").update(pass).digest("hex");
+  return createHash("sha256").update(pass).digest("hex");
 }
 
 function checkPass(user, pass) {
-	const validity = user?.pass === pass ? true : false;
-	return validity;
+  const validity = user?.pass === hashPass(pass) ? true : false;
+  return validity;
 }
 
 module.exports.hashPass = hashPass;
