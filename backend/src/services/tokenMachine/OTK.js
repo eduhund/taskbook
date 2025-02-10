191f485f3effdeddbc9d80f70556aec6526e7f4a
Roman Nebel <im.roman.nebel@gmail.com>
2025-02-10 15:02:17 +0100

    Fix pass hash

--- backend/src/services/tokenMachine/OTK.js
+++ backend/src/services/tokenMachine/OTK.js
@@ -2,59 +2,65 @@ const fetch = require("node-fetch");
 
 const { OTK_SERVER, OTK_TOKEN } = process.env;
 
-const bearerToken = "Bearer " + OTK_TOKEN
+const bearerToken = "Bearer " + OTK_TOKEN;
 
 function getKeyParams(type) {
-	switch (type) {
-		case "oneTimePass":
-			return {
-				type: "digit",
-				length: 4,
-				life: 3600,
-				isReusable: false
-			}
-		case "oneTimeKey":
-			return {
-				type: "string",
-				length: 8,
-				life: 62 * 24 * 60 * 60,
-				isReusable: false
-			}
-		default:
-			return {
-				type: "digit",
-				length: 4,
-				life: 3600,
-				isReusable: false
-			}
-	}
+  switch (type) {
+    case "oneTimePass":
+      return {
+        type: "digit",
+        length: 4,
+        life: 3600,
+        isReusable: false,
+      };
+    case "oneTimeKey":
+      return {
+        type: "string",
+        length: 8,
+        life: 62 * 24 * 60 * 60,
+        isReusable: false,
+      };
+    default:
+      return {
+        type: "digit",
+        length: 4,
+        life: 3600,
+        isReusable: false,
+      };
+  }
 }
 
 async function checkKey(key) {
-	const response = await fetch(`${OTK_SERVER}/checkKey?key=${key}`, {
-		headers: { 
-			"Authorization": bearerToken
-		 },
-	});
-	const data = await response.json();
-	return data?.OK || false;
+  try {
+    const response = await fetch(`${OTK_SERVER}/checkKey?key=${key}`, {
+      headers: {
+        Authorization: bearerToken,
+      },
+    });
+    const { OK, error } = await response.json();
+    console.log(error);
+    return OK;
+  } catch (e) {
+    console.log(e);
+    return false;
+  }
 }
 
 async function setKey(userId, type) {
-	const body = {
-		userId,
-		...getKeyParams(type)
-	}
-	const response = await fetch(`${OTK_SERVER}/setKey`, {
-		method: "POST",
-		body: JSON.stringify(body),
-		headers: { 
-			"Content-Type": "application/json",
-			"Authorization": bearerToken
-		 },
-	});
-	const data = await response.json();
-	return data?.data?.key || undefined;
+  const body = {
+    userId,
+    ...getKeyParams(type),
+  };
+  const response = await fetch(`${OTK_SERVER}/setKey`, {
+    method: "POST",
+    body: JSON.stringify(body),
+    headers: {
+      "Content-Type": "application/json",
+      Authorization: bearerToken,
+    },
+  });
+  const data = await response.json();
+  return data?.data?.key || undefined;
 }
 
 module.exports = { checkKey, setKey };
