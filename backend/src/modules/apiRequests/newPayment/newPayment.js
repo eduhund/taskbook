b18d36def4fc7347b09b7f55ecf74b553f887a1f
Roman Nebel <im.roman.nebel@gmail.com>
2025-02-12 13:06:59 +0100

    Update payment method

--- backend/src/modules/apiRequests/newPayment/newPayment.js
+++ backend/src/modules/apiRequests/newPayment/newPayment.js
@@ -1,73 +1,9 @@
 const { getDBRequest } = require("../../dbRequests/dbRequests");
-const { createUser } = require("../createUser/createUser");
 const { calculateDeadline } = require("../../../utils/calculators");
 const { hashPass } = require("../../../utils/pass");
 const { setKey } = require("../../../services/tokenMachine/OTK");
-const { log } = require("../../../services/logger/logger");
 const { sendMail } = require("../../../services/mailer");
-
-function checkSource(body) {
-  const keys = Object.keys(body);
-
-  if (keys.includes("gumroad_fee")) {
-    return "Gumroad";
-  }
-
-  if (keys.includes("sign")) {
-    return "Tilda";
-  }
-
-  return undefined;
-}
-
-function splitName(name) {
-  if (name) {
-    const [firstName, lastName] = name.split(" ", 2);
-    return {
-      firstName,
-      lastName,
-    };
-  } else return {};
-}
-
-function prepareData(source, data, date) {
-  const initData = { source, ts: Date.now() };
-  const products = {
-    ["_-jg_yvw1calvFhRDQaqJg=="]: "HSE",
-    ["DYGiKngRwU-N1dt_WOJ0lg=="]: "HSP",
-  };
-
-  switch (source) {
-    case "Gumroad":
-      return {
-        ...initData,
-        date,
-        email: String(data.email).toLowerCase(),
-        firstName: data["First Name"],
-        lastName: data["Last Name"],
-        paymentId: data.sale_id,
-        moduleId: products[data.product_id],
-        value: data.price,
-        currency: "USD",
-      };
-    case "Tilda":
-      const moduleId = data.payment.products[0].sku.slice(0, 3);
-      const isProlongation = data.payment.products[0].sku.includes("+");
-      return {
-        ...initData,
-        date,
-        email: String(data.email).toLowerCase(),
-        ...splitName(data.name),
-        paymentId: data.payment.orderid,
-        moduleId,
-        isProlongation,
-        value: data.payment.products[0].price,
-        currency: "RUB",
-      };
-    default:
-      throw new Error("Payment data wasn't prepared");
-  }
-}
+const { log } = require("@logger");
 
 function getDateObject(date) {
   const timestamp = date ? Date.parse(date) : Date.mow();
@@ -80,216 +16,207 @@ function getISODateOny(date) {
 }
 
 async function newPayment(req, res) {
-  const { body } = req;
+  try {
+    const { body } = req;
 
-  if (body.test) {
-    res.sendStatus(200);
-    return;
-  }
+    const {
+      email,
+      firstName,
+      lastName,
+      startDate,
+      moduleId,
+      isProlongation,
+      transactionId,
+    } = body;
 
-  log.debug(body);
-  const date = new Date(Date.now());
+    const now = new Date();
 
-  const source = checkSource(body);
-
-  if (!source) {
-    res.status(400);
-    res.send({
-      OK: false,
-      error: "invalid_params",
+    const moduleInfo = await getDBRequest("getModuleInfo", {
+      query: { code: moduleId.toUpperCase() },
     });
-    return;
-  }
 
-  const payment = prepareData(source, body, date);
-
-  log.debug(payment);
-
-  const moduleInfo = await getDBRequest("getModuleInfo", {
-    query: { code: payment.moduleId.toUpperCase() },
-  });
-
-  if (!moduleInfo) {
-    res.status(400);
-    res.send({
-      OK: false,
-      error: "invalid_params",
-    });
-    return;
-  }
-
-  const user = await getDBRequest("getUserInfo", {
-    query: { email: payment.email },
-  });
-
-  let start = getISODateOny(date);
-
-  if (body.start) {
-    const dateArray = body.start.split(".");
-    const startDateNormilized = `${dateArray[2]}-${dateArray[1]}-${dateArray[0]}`;
-    start = getISODateOny(startDateNormilized);
-  }
-  let deadline = calculateDeadline(start, 62);
-
-  if (!user) {
-    const userPass = Math.random().toString(36).substring(2);
-    const lang = source === "Tilda" ? "ru" : "en";
-
-    const newUser = {
-      email: payment.email,
-      pass: userPass ? hashPass(userPass) : "",
-      firstName: payment.firstName,
-      lastName: payment.lastName,
-      modules: {
-        [payment.moduleId]: {
-          start,
-          deadline,
-          prolongations: [],
-        },
-      },
-      lang,
-    };
-
-    const createdUser = await getDBRequest("addUser", newUser);
-
-    const secureKey = await setKey(createdUser?.id, "oneTimeKey");
-
-    const link = `${process.env.FRONTEND_URL}/createPassword?email=${payment.email}&verifyKey=${secureKey}&lang=${lang}`;
-
-    const params = {
-      template_id: getISODateOny(date) < start ? "rq4lt12h" : "l9ujirgq",
-      address: payment.email,
-      lang,
-    };
-
-    const data = {
-      NAME: newUser?.firstName || "",
-      MODULE: moduleInfo?.name || "",
-      MODULELINK: moduleInfo?.moduleLink || "",
-      MASCOTLETTERTOP: moduleInfo?.mascot?.letterTop || "",
-      MASCOTLETTERBOTTOM: moduleInfo?.mascot?.letterBottom || "",
-      START_DATE: new Date(Date.parse(start)).toLocaleDateString("ru-RU", {
-        month: "long",
-        day: "numeric",
-      }),
-      PASSWORD: userPass || "",
-      PASSWORD_LINK: link,
-    };
-
-    await sendMail(params, data);
-  } else if (payment?.isProlongation) {
-    const now = new Date(Date.now());
-    const currentDeadline = new Date(user?.modules[payment?.moduleId].deadline);
-    const newDeadline = currentDeadline > now ? currentDeadline : now;
-    deadline = calculateDeadline(newDeadline, 62);
-
-    const prolData = {
-      type: "renewal",
-      paymentId: payment.paymentId,
-      until: deadline,
-    };
-
-    user.modules[payment?.moduleId].deadline = deadline;
-    if (!Array.isArray(user.modules[payment?.moduleId].prolongations)) {
-      user.modules[payment?.moduleId].prolongations = [];
+    if (!moduleInfo) {
+      res.status(400);
+      res.send({
+        OK: false,
+        error: "invalid_params",
+      });
+      return;
     }
-    user.modules[payment?.moduleId].prolongations.push(prolData);
 
-    await getDBRequest("setUserInfo", {
-      id: user?.id,
-      data: { modules: user.modules },
+    const user = await getDBRequest("getUserInfo", {
+      query: { email: email },
     });
 
-    const params = {
-      template_id: "c8oikgth",
-      address: payment.email,
-      lang,
-    };
+    let deadline = calculateDeadline(startDate, 62);
 
-    const data = {
-      NAME: user?.firstName || "",
-      MODULE: moduleInfo?.name || "",
-      MODULELINK: moduleInfo?.moduleLink || "",
-      MASCOTLETTERTOP: moduleInfo?.mascot?.letterTop || "",
-      MASCOTLETTERBOTTOM: moduleInfo?.mascot?.letterBottom || "",
-      START_DATE: new Date(Date.parse(start)).toLocaleDateString("ru-RU", {
-        month: "long",
-        day: "numeric",
-      }),
-    };
+    const lang = moduleId === "HSE" || moduleId === "HSP" ? "en" : "ru";
 
-    await sendMail(params, data);
-  } else {
-    const activeModules = Object.entries(user.modules || {}).filter(
-      ([, value]) =>
-        Date.now() - Date.parse(value.deadline) <= 7 * 24 * 60 * 60 * 1000
-    );
+    if (!user) {
+      const userPass = Math.random().toString(36).substring(2);
 
-    for (const [id] of activeModules) {
-      const isCurrent = id === payment?.moduleId;
-      const type = isCurrent ? "newBuy" : "otherModule";
+      const newUser = {
+        email: email,
+        pass: userPass ? hashPass(userPass) : "",
+        firstName: firstName,
+        lastName: lastName,
+        modules: {
+          [moduleId]: {
+            start: startDate,
+            deadline,
+            prolongations: [],
+          },
+        },
+        lang,
+      };
+
+      const createdUser = await getDBRequest("addUser", newUser);
+
+      const secureKey = await setKey(createdUser?.id, "oneTimeKey");
+
+      const link = `${process.env.FRONTEND_URL}/createPassword?email=${email}&verifyKey=${secureKey}&lang=${lang}`;
+
+      const params = {
+        template_id: getISODateOny(now) < startDate ? "rq4lt12h" : "l9ujirgq",
+        address: email,
+        lang,
+      };
+
       const data = {
-        type,
-        paymentId: payment.paymentId,
+        NAME: newUser?.firstName || "",
+        MODULE: moduleInfo?.name || "",
+        MODULELINK: moduleInfo?.moduleLink || "",
+        MASCOTLETTERTOP: moduleInfo?.mascot?.letterTop || "",
+        MASCOTLETTERBOTTOM: moduleInfo?.mascot?.letterBottom || "",
+        START_DATE: new Date(Date.parse(startDate)).toLocaleDateString(
+          "ru-RU",
+          {
+            month: "long",
+            day: "numeric",
+          }
+        ),
+        PASSWORD: userPass || "",
+        PASSWORD_LINK: link,
+      };
+
+      await sendMail(params, data);
+    } else if (isProlongation) {
+      const now = new Date(Date.now());
+      const currentDeadline = new Date(user?.modules[moduleId].deadline);
+      const newDeadline = currentDeadline > now ? currentDeadline : now;
+      deadline = calculateDeadline(newDeadline, 62);
+
+      const prolData = {
+        type: "renewal",
+        transactionId,
         until: deadline,
       };
 
-      user.modules[id].deadline = deadline;
-      if (!Array.isArray(user.modules[id].prolongations)) {
-        user.modules[id].prolongations = [];
+      user.modules[moduleId].deadline = deadline;
+      if (!Array.isArray(user.modules[moduleId].prolongations)) {
+        user.modules[moduleId].prolongations = [];
       }
-      user.modules[id].prolongations.push(data);
-    }
+      user.modules[moduleId].prolongations.push(prolData);
 
-    if (!user.modules[payment.moduleId]) {
-      user.modules[payment.moduleId] = {
-        start,
-        deadline,
-        prolongations: [
-          {
-            type: "newBuy",
-            paymentId: payment.paymentId,
-            until: deadline,
-          },
-        ],
+      await getDBRequest("setUserInfo", {
+        id: user?.id,
+        data: { modules: user.modules },
+      });
+
+      const params = {
+        template_id: "c8oikgth",
+        address: email,
+        lang,
       };
+
+      const data = {
+        NAME: user?.firstName || "",
+        MODULE: moduleInfo?.name || "",
+        MODULELINK: moduleInfo?.moduleLink || "",
+        MASCOTLETTERTOP: moduleInfo?.mascot?.letterTop || "",
+        MASCOTLETTERBOTTOM: moduleInfo?.mascot?.letterBottom || "",
+        START_DATE: new Date(Date.parse(startDate)).toLocaleDateString(
+          "ru-RU",
+          {
+            month: "long",
+            day: "numeric",
+          }
+        ),
+      };
+
+      await sendMail(params, data);
+    } else {
+      const activeModules = Object.entries(user.modules || {}).filter(
+        ([, value]) =>
+          Date.now() - Date.parse(value.deadline) <= 7 * 24 * 60 * 60 * 1000
+      );
+
+      for (const [id] of activeModules) {
+        const isCurrent = id === moduleId;
+        const type = isCurrent ? "newBuy" : "otherModule";
+        const data = {
+          type,
+          transactionId,
+          until: deadline,
+        };
+
+        user.modules[id].deadline = deadline;
+        if (!Array.isArray(user.modules[id].prolongations)) {
+          user.modules[id].prolongations = [];
+        }
+        user.modules[id].prolongations.push(data);
+      }
+
+      if (!user.modules[moduleId]) {
+        user.modules[moduleId] = {
+          start: startDate,
+          deadline,
+          prolongations: [
+            {
+              type: "newBuy",
+              transactionId,
+              until: deadline,
+            },
+          ],
+        };
+      }
+
+      await getDBRequest("setUserInfo", {
+        id: user?.id,
+        data: { modules: user.modules },
+      });
+
+      const params = {
+        template_id: getISODateOny(now) < startDate ? "ju2fou81" : "it2mrnd7",
+        address: email,
+        lang,
+      };
+
+      const data = {
+        NAME: user?.firstName || "",
+        MODULE: moduleInfo?.name || "",
+        MODULELINK: moduleInfo?.moduleLink || "",
+        MASCOTLETTERTOP: moduleInfo?.mascot?.letterTop || "",
+        MASCOTLETTERBOTTOM: moduleInfo?.mascot?.letterBottom || "",
+        START_DATE: new Date(Date.parse(startDate)).toLocaleDateString(
+          "ru-RU",
+          {
+            month: "long",
+            day: "numeric",
+          }
+        ),
+      };
+
+      await sendMail(params, data);
     }
 
-    await getDBRequest("setUserInfo", {
-      id: user?.id,
-      data: { modules: user.modules },
-    });
-
-    const params = {
-      template_id: getISODateOny(date) < start ? "ju2fou81" : "it2mrnd7",
-      address: payment.email,
-      lang,
-    };
-
-    const data = {
-      NAME: user?.firstName || "",
-      MODULE: moduleInfo?.name || "",
-      MODULELINK: moduleInfo?.moduleLink || "",
-      MASCOTLETTERTOP: moduleInfo?.mascot?.letterTop || "",
-      MASCOTLETTERBOTTOM: moduleInfo?.mascot?.letterBottom || "",
-      START_DATE: new Date(Date.parse(start)).toLocaleDateString("ru-RU", {
-        month: "long",
-        day: "numeric",
-      }),
-    };
-
-    await sendMail(params, data);
-  }
-
-  const response = await getDBRequest("setPayment", { payment });
-
-  if (response?.acknowledged) {
     res.sendStatus(200);
-  } else {
-    res.sendStatus(400);
+    return;
+  } catch (e) {
+    log.error("Error in new payment processing!");
+    log.debug(e);
+    res.sendStatus(500);
   }
-
-  return;
 }
 
 module.exports = newPayment;
--- backend/src/services/logger/logger.js
+++ backend/src/services/logger/logger.js
@@ -1,22 +1,22 @@
 const log4js = require("log4js");
 
 log4js.configure({
-	appenders: {
-		out: { type: "stdout" },
-		file: { type: "file", filename: "./logs/eduhund-backend.log" },
-		slack: {
-			type: "@log4js-node/slack",
-			token: process.env.SLACK_BOT_TOKEN,
-			channel_id: process.env.SLACK_CHANNEL_ID,
-			username: "ѢѢ",
-		},
-	},
-	categories: {
-		default: { appenders: ["out"], level: "debug" },
-		local: { appenders: ["out", "file"], level: "debug" },
-		test: { appenders: ["out", "file"], level: "debug" },
-		prod: { appenders: ["out", "file", "slack"], level: "debug" },
-	},
+  appenders: {
+    out: { type: "stdout" },
+    file: { type: "file", filename: "./logs/eduhund-backend.log" },
+    slack: {
+      type: "@log4js-node/slack",
+      token: process.env.SLACK_BOT_TOKEN,
+      channel_id: process.env.SLACK_CHANNEL_ID,
+      username: "Taskbook",
+    },
+  },
+  categories: {
+    default: { appenders: ["out"], level: "debug" },
+    local: { appenders: ["out", "file"], level: "debug" },
+    test: { appenders: ["out", "file"], level: "debug" },
+    prod: { appenders: ["out", "file", "slack"], level: "debug" },
+  },
 });
 
 const log = log4js.getLogger(process.env.MACHINE);
