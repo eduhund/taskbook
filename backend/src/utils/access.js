57e79d9bbe14049b96315d3b5fcae67ea14bf48c
Roman Nebel <r@nebel.im>
2025-09-09 09:58:09 +0200

    Improve readability of getDeadline function by enhancing sorting logic and formatting

--- backend/src/utils/access.js
+++ backend/src/utils/access.js
@@ -8,15 +8,20 @@
  * @returns {String} Date of deadline
  */
 function getDeadline({ deadline, prolongations = [] }) {
-	if (prolongations.length === 0) return deadline;
+  if (prolongations.length === 0) return deadline;
 
-	if (prolongations.length > 1) {
-		prolongations.sort((a, b) => Date.parse(b.until) - Date.parse(a.until));
-	}
+  if (prolongations.length > 1) {
+    prolongations.sort((a, b) => {
+      if (!a?.until && !b?.until) return 0;
+      if (!a?.until) return 1;
+      if (!b?.until) return -1;
+      return Date.parse(b.until) - Date.parse(a.until);
+    });
+  }
 
-	return prolongations[0]?.until > deadline
-		? prolongations[0]?.until
-		: deadline;
+  return prolongations[0]?.until > deadline
+    ? prolongations[0]?.until
+    : deadline;
 }
 
 module.exports = { getDeadline };
