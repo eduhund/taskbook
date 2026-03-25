512f030b1d339b76b2706c9160e2367f52a184f1
Roman Nebel <r@nebel.im>
2026-03-25 14:21:35 +0100

    Remove addUser logging from database method

--- backend/src/modules/dbRequests/addUser/addUser.js
+++ backend/src/modules/dbRequests/addUser/addUser.js
@@ -1,8 +1,6 @@
-const { log } = require("../../../services/logger/logger");
 const { USERS } = require("../../../services/mongo/mongo");
 
 async function addUser(user) {
-  log.info("Registering user:", user);
   return USERS.countDocuments().then((totalUsers) => {
     const userId = `U${(totalUsers + 1).toString().padStart(7, "0")}`;
     user.id = userId;
